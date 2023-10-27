ad7112b71276c1c4449e076f97b3793caab95efd
Dokoye98 <dwayneokoye98@gmail.com>
2023-10-27 22:54:04 +0100

    validation file added for signup and login process

--- models/Users.js
+++ models/Users.js
@@ -2,15 +2,15 @@
 const mongoose = require('mongoose')
 //in this schema i decided to add first and last name since its a twitter style social media
 const UserSchema = mongoose.Schema({
-
-    FirstName:{
+    //schema will not follow camelcase due to being case sensitive
+    firstName:{
         type:String,
         required:true,
         min:3,
         max:25
 
     },
-    LastName:{
+    lastName:{
         type:String,
         required:true,
         min:3,
---
+++ validation/validation.js
@@ -0,0 +1,34 @@
+//one import the joi for validation of schema
+
+const joi = require('joi')
+
+
+//for now there will be a signup validation and login validation
+//each require a joi object
+
+const signUpValidation = (data)=>{
+    //the validation will follow a similar naming scheme because it is a schema validation
+    const schemaValidation = joi.object({
+       
+        firstname:joi.string().required().min(3).max(256),
+        lastname:joi.string().required().min(3).max(256),
+        username:joi.string().required().min(6).max(256),
+        email:joi.string().required().min(6).max(256).email(),
+        password:joi.string().required().min(6).max(256)
+    })
+    return schemaValidation.validate(data)
+}
+
+//the login validation will be different as it will only require the username and password
+
+const loginValidation = (data)=>{
+    const schemaValidation = joi.object({
+
+        username:joi.string().required().min(6).max(256),
+        password:joi.string().required().min(6).max(256)
+    })
+    return schemaValidation.validate(data)
+}
+
+module.exports.signUpValidation = signUpValidation
+module.exports.loginValidation = loginValidation
